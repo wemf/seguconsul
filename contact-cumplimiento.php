339e44fb60f3b8264ef5dec228e09c8687312181
William Eduardo Meza <williameduardomeza@gmail.com>
2017-10-16 20:35:42 -0500

    refactor de site to simple php

---
+++ contact-cumplimiento.php
@@ -0,0 +1,196 @@
+<html>
+
+    <head>
+
+        <title>Contact Form Tutorial by Bootstrapious.com</title>
+
+        <meta charset="UTF-8">
+
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
+
+        <link href='https://fonts.googleapis.com/css?family=Lato:300,400,700' rel='stylesheet' type='text/css'>
+
+        <link href='css/contact.css' rel='stylesheet' type='text/css'>
+
+    </head>
+
+    <body>
+
+
+
+        <div class="container">
+
+
+
+            <div class="row">
+
+
+
+                <div class="col-lg-8 col-lg-offset-2">
+
+
+
+                    <h1>Cumplimiento</h1>
+
+
+
+                    <p class="lead">Aseguraremos tus contratos de obra, suministros, prestación de servicios, etc Al mejor Costo-Beneficio.</p>
+
+                    <?php
+                    if(isset($_GET['msg']) and $_GET['msg']=='success') {
+                    ?>
+                    <div class="col-lg-12 col-sm-12 col-md-12">
+                        <div class="alert alert-success">
+                            <strong>Su datos fueron enviados correctamente, gracias por su tiempo, le estaremos contactando.</strong>
+                        </div>
+                    </div>
+                    <?php
+                    }
+                    ?>
+
+
+
+                    <form id="contact-form" method="post" action="contact.php" role="form">
+
+
+
+                        <div class="messages"></div>
+
+
+
+                        <div class="controls">
+
+
+
+                            <div class="row">
+
+                                <div class="col-md-6">
+
+                                    <div class="form-group">
+
+                                        <label for="form_name">Nombre *</label>
+
+                                        <input id="form_name" type="text" name="name" class="form-control" placeholder="Ingrese su nombre *" required="required" data-error="Firstname is required.">
+
+                                        <div class="help-block with-errors"></div>
+
+                                    </div>
+
+                                </div>
+
+                                <div class="col-md-6">
+
+                                    <div class="form-group">
+
+                                        <label for="form_lastname">Apellido *</label>
+
+                                        <input id="form_lastname" type="text" name="surname" class="form-control" placeholder="Ingrese su apellido *" required="required" data-error="Lastname is required.">
+
+                                        <div class="help-block with-errors"></div>
+
+                                    </div>
+
+                                </div>
+
+                            </div>
+
+                            <div class="row">
+
+                                <div class="col-md-6">
+
+                                    <div class="form-group">
+
+                                        <label for="form_email">Email *</label>
+
+                                        <input id="form_email" type="email" name="email" class="form-control" placeholder="Ingrese su email *" required="required" data-error="Valid email is required.">
+
+                                        <div class="help-block with-errors"></div>
+
+                                    </div>
+
+                                </div>
+
+                                <div class="col-md-6">
+
+                                    <div class="form-group">
+
+                                        <label for="form_phone">Teléfono</label>
+
+                                        <input id="form_phone" type="tel" name="phone" class="form-control" placeholder="Ingrese su número telefónico">
+
+                                        <div class="help-block with-errors"></div>
+
+                                    </div>
+
+                                </div>
+
+                            </div>
+
+                            <div class="row">
+
+                                <div class="col-md-12">
+
+                                    <div class="form-group">
+
+                                        <label for="form_message">Mensaje *</label>
+
+                                        <textarea id="form_message" name="message" class="form-control" placeholder="Mensaje *" rows="4" required="required" data-error="Please,leave us a message."></textarea>
+
+                                        <div class="help-block with-errors"></div>
+
+                                    </div>
+
+                                </div>
+
+                                <div class="col-md-12">
+
+                                    <input type="submit" class="btn btn-success btn-send" value="Enviar mensaje">
+
+                                </div>
+
+                            </div>
+
+                            <div class="row">
+
+                                <div class="col-md-12">
+
+                                    <p class="text-muted"><strong>*</strong>Estos campos son requeridos</p>
+
+                                </div>
+
+                            </div>
+
+                        </div>
+
+
+
+                    </form>
+
+
+
+                </div><!-- /.8 -->
+
+
+
+            </div> <!-- /.row-->
+
+
+
+        </div> <!-- /.container-->
+
+
+
+        <script src="js/jquery.js"></script>
+
+        <script src="js/bootstrap.min.js"></script>
+
+        <script src="js/validator.js"></script>
+
+        <script src="js/contact.js"></script>
+
+    </body>
+
+</html>
+
--- contact.php
+++ contact.php
@@ -1,86 +1,44 @@
 <?php
-require './vendor/phpmailer/phpmailer/PHPMailerAutoload.php';
-require './config/config.php';
-$mail = new PHPMailer;
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-	//Sanitaze inputs
-    $name    = stripslashes(trim($_POST['name']));
-    $email   = stripslashes(trim($_POST['email']));
-    $surname   = stripslashes(trim($_POST['surname']));
-    $phone   = stripslashes(trim($_POST['phone']));
-    $message   = stripslashes(trim($_POST['message']));
-    
-    $pattern = '/[\r\n]|Content-Type:|Bcc:|Cc:/i';
-    if (preg_match($pattern, $name) || preg_match($pattern, $email)) {
-		die("Header injection detected");
-    }
-    $emailIsValid = filter_var($email, FILTER_VALIDATE_EMAIL);
-    if ($name && $email && $emailIsValid) {
-		//Tell PHPMailer to use SMTP
-		$mail->isSMTP();
-		$mail->SMTPOptions = array('ssl' => array( 'verify_peer' => false, 'verify_peer_name' => false, 'allow_self_signed' => true));
-		//Set the hostname of the mail server
-		$mail->Host = 'uscentral447.accountservergroup.com';
-		//Set the SMTP port number - 587 for authenticated TLS, a.k.a. RFC4409 SMTP submission
-		$mail->Port = 465;
-		//Set the encryption system to use - ssl (deprecated) or tls
-		$mail->SMTPSecure = 'tls';
-		//Whether to use SMTP authentication
-		$mail->SMTPAuth = true;
-		//Username to use for SMTP authentication - use full email address for gmail
-		$mail->Username = user;
-		//Password to use for SMTP authentication
-		$mail->Password = pass;
-        $mail->addAddress(to);
-		//Set who the message is to be sent from
-		$mail->setFrom(from, from);
-		//Set email Subject
-		$mail->Subject = prefix;
-		//Set email body content
-        $body = "
-        <!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">
-        <html>
-            <head>
-                <meta charset=\"utf-8\">
-            </head>
-            <body>
-                <h1>Cliente registrado del formulario de contacto:</h1>
-                <p><strong>Nombre:</strong> {$name}</p>
-                <p><strong>Apellido:</strong> {$surname}</p>
-                <p><strong>Email:</strong> {$email}</p>
-                <p><strong>Telf:</strong> {$phone}</p>
-                <p><strong>Mensaje:</strong> {$message}</p>
-            </body>
-		</html>";
-		$mail->isHTML(true);
-		$mail->Body = $body;
+$name    = stripslashes(trim($_POST['name']));
+$email   = stripslashes(trim($_POST['email']));
+$surname   = stripslashes(trim($_POST['surname']));
+$phone   = stripslashes(trim($_POST['phone']));
+$message   = stripslashes(trim($_POST['message']));
+	
+$to = "contacto@seguconsul.com";
+$subject = "Solicitud de Contacto desde Seguconsul";
 
+$message = "
+<html>
+<head>
+<title>Nueva solicitud</title>
+</head>
+<body>
+<p>This email contains HTML Tags!</p>
+<table>
+<tr>
+<h1>Cliente registrado del formulario de contacto:</h1>
+<p><strong>Nombre:</strong> {$name}</p>
+<p><strong>Apellido:</strong> {$surname}</p>
+<p><strong>Email:</strong> {$email}</p>
+<p><strong>Telf:</strong> {$phone}</p>
+<p><strong>Mensaje:</strong> {$message}</p>
+</tr>
+</table>
+</body>
+</html>
+";
 
-$okMessage = 'Mensaje enviado correctamente. Pronto nos pondremos en contacto con usted.';
-$errorMessage = 'Hubo un error durante el envío del mensaje. Por favor intente más tarde.';
-$responseArray = array('type' => 'success', 'message' => $okMessage);
+// Always set content-type when sending HTML email
+$headers = "MIME-Version: 1.0" . "\r\n";
+$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
 
-		//Send email, save data on users_emails.txt and show error if not
-		if(!$mail->send()) {
-			$hasError = true;
-			$msgError = $mail->ErrorInfo;
-$encoded = json_encode($responseArray);
+// More headers
+$headers .= 'From: <admin@seguconsul.com>' . "\r\n";
+$headers .= 'Cc: williameduardomeza@gmail.com' . "\r\n";
 
-    header('Content-Type: application/json');
+mail($to,$subject,$message,$headers);
 
-    echo $encoded;
-		}
-		else {
-			$emailSent = true;
-			$fp = fopen('users_emails.txt', 'a');
-			fwrite($fp, 'Nombre: '.$name.'; Email: '.$email.';'."\n");
-			fclose($fp);
-$encoded = json_encode($responseArray);
-
-    header('Content-Type: application/json');
-
-    echo $encoded;
-		}
-    } 
-}
+header('Location: ' . $_SERVER['HTTP_REFERER'].'?msg=success');
+die();
 ?>
